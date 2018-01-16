ecb25321dda3458ef486bccb0a8311fd3768f426
alumno <alumno@DESKTOP-FB4IIJD>
2018-01-16 10:03:46 +0100

    Commit1

---
+++ prueba1.php
@@ -0,0 +1,16 @@
+<?php
+$esprimo=TRUE;
+$count=0;
+for ($numero=1; $count<10; $numero++) {
+    for ($j=2;$j<$numero;$j++) {
+        if ($numero%$j==0) {
+            $esprimo=false;
+        }
+    }
+    if ($esprimo){
+        echo $numero."</br>";
+        $count++;
+    }
+    $esprimo=TRUE;
+}
+?>
